e664b794f543e872b15008eedac2a5e796d1fb55
Ivan Mysin <imysin@mail.ru>
2023-09-17 13:14:10 +0300

    Scipy example

--- scipy_int_example.py
+++ scipy_int_example.py
@@ -11,14 +11,34 @@ def func(t, Y, sigma, rho, beta):
 
     dYdt = [dxdt, dydt, dzdt]
     return dYdt
-###########################
+#############################
+
+class Diff_Eqs:
+    def __init__(self, sigma, rho, beta):
+        self.sigma = sigma
+        self.rho = rho
+        self.beta = beta
+
+    def func(self, t, Y):
+        x, y, z = Y
+        dxdt = self.sigma * (y - x)
+        dydt = x * (self.rho - z) - y
+        dzdt = x * y - self.beta * z
+
+        dYdt = [dxdt, dydt, dzdt]
+        return dYdt
+##############################
+
 sigma = 10
 rho = 28
 beta = 8/3
 y0 = [2.0, 1.0, 1.0]
 t_span = [0, 100]
 t_eval = np.linspace(0, 100, 1000)
-sol = solve_ivp(func, t_span, y0, method='LSODA',args=(sigma, rho, beta), t_eval=t_eval, rtol=10e-6)
+#sol = solve_ivp(func, t_span, y0, method='LSODA', args=(sigma, rho, beta), t_eval=t_eval, rtol=10e-6)
+
+system = Diff_Eqs(sigma, rho, beta)
+sol = solve_ivp(system.func, t_span, y0, method='LSODA', t_eval=t_eval, rtol=10e-6)
 
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
